97b76cbdd61f120844f0a69ae5613329d51afb55
harsha16208 <sriharsha16208@gmail.com>
2023-08-22 18:41:23 +0530

    Create 88.MergeSortedArray.py

---
+++ 88.MergeSortedArray.py
@@ -0,0 +1,36 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if len(nums1) == 0:
+            return nums2
+        if len(nums2) == 0:
+            return nums1
+        c1 = c2 = 0
+        
+        #copy numbers of nums1 to a temp array
+        temp = []
+        for i in nums1:
+            temp.append(i)
+
+        #remove all elements from nums1
+        for i in range(0, len(nums1)):
+            nums1.pop()
+
+        while c1 < m and c2 < n:
+            if temp[c1] < nums2[c2]:
+                nums1.append(temp[c1])
+                c1+=1
+            else:
+                nums1.append(nums2[c2])
+                c2+=1
+        
+        #Add remaining elements if any
+        if c1 < m:
+            for i in range(c1, m):
+                nums1.append(temp[i])
+        
+        if c2 < n:
+            for i in range(c2, n):
+                nums1.append(nums2[i])
