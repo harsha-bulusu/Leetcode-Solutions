8f203d303c4cec4b5d2eacc6303d9944a6269ba5
harsha16208 <sriharsha16208@gmail.com>
2023-08-29 11:13:58 +0530

    Update 100.SameTree.py

--- 100.SameTree.py
+++ 100.SameTree.py
@@ -13,8 +13,4 @@ class Solution:
             return False
         
         if p.val == q.val:
-            l_same = self.isSameTree(p.left, q.left)
-            r_same = self.isSameTree(p.right, q.right)
-            return l_same and r_same
-
-        
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
