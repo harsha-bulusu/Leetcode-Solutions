158af22d0db7d255e18b67324ccfa4b810c01d88
harsha16208 <sriharsha16208@gmail.com>
2023-09-08 10:22:03 +0530

    Create 450.DeleteNodeInBST.java

---
+++ 450.DeleteNodeInBST.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    TreeNode inorderSuccessor(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            if (root.left == null) {
+                return root.right;
+            }
+
+            if(root.right == null) {
+                return root.left;
+            }
+
+            TreeNode is = inorderSuccessor(root.right);
+            root.val = is.val;
+            root.right = deleteNode(root.right, is.val);
+        }
+
+        return root;
+
+    }
+}
