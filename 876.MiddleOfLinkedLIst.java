c9262de4d8e8cb57601b184768f0450e32f49754
harsha16208 <sriharsha16208@gmail.com>
2023-10-20 10:41:59 +0530

    Create 876.MiddleOfLinkedLIst.java

---
+++ 876.MiddleOfLinkedLIst.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    int len(ListNode head) {
+        ListNode cursor = head;
+        int len = 0;
+        while(cursor != null) {
+            len++;
+            cursor = cursor.next;
+        }
+        return len;
+    }
+
+    public ListNode middleNode(ListNode head) {
+        int len = len(head);
+        int mid = len / 2;
+        ListNode cursor = head;
+        for (int i=0; i<mid; i++) {
+            cursor = cursor.next;
+        }
+        return cursor;
+    }
+}
