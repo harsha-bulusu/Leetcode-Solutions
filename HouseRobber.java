15bb7640d6ce89989a4b94e8c4550af1de22a6cd
harsha_bulusu <sriharsha16208@gmail.com>
2024-01-21 21:36:56 +0530

    Update HouseRobber.java

--- HouseRobber.java
+++ HouseRobber.java
@@ -10,10 +10,37 @@ class Solution {
         return dp[index] = Math.max(take, notTake);
     }
 
+    int tabulate(int []nums, int n) {
+        // int dp[] = new int[n];
+
+        // base-case
+        // dp[0] = nums[0];
+        int prev = nums[0];
+        int prev2 = 0;
+
+        //represent states
+        for (int i = 1; i < n; i++) {
+            int take = nums[i];
+            if (i > 1)
+                // take += dp[i - 2];
+                take += prev2;
+            // int notTake = 0 + dp[i - 1];
+            int notTake = 0 + prev;
+
+            // dp[i] = Math.max(take, notTake);
+            int curr = Math.max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+        // return dp[n - 1];
+        return prev;
+    }
+
     public int rob(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return solve(n - 1, nums, dp);
+        // int dp[] = new int[n];
+        // Arrays.fill(dp, -1);
+        // return solve(n - 1, nums, dp);
+        return tabulate(nums, n);
     }
 }
