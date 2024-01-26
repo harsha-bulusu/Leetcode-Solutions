dced3e8b82acb327596d0e281c1fb51bc503184b
harsha_bulusu <sriharsha16208@gmail.com>
2024-01-26 21:13:29 +0530

    Update OutOfBoundaryPaths.java

--- OutOfBoundaryPaths.java
+++ OutOfBoundaryPaths.java
@@ -5,16 +5,21 @@ class Solution {
     public int solve(int i, int j, int maxMove, int m, int n, int[][][] dp) {
         //base case
         if (i < 0 || j < 0 || i >= m || j>= n) return 1;
-        if (maxMove == 0) return 0;
         if (dp[i][j][maxMove] != -1) return dp[i][j][maxMove];
         
         //operations
-        int up = solve(i - 1, j, maxMove - 1, m, n, dp) % mod;
-        int down = solve(i + 1, j, maxMove - 1, m, n, dp) % mod;
-        int left = solve(i, j - 1, maxMove - 1, m, n, dp) % mod;
-        int right = solve(i, j + 1, maxMove - 1, m, n, dp) % mod;
-
-        return dp[i][j][maxMove] = ((up + left) % mod + (down + right) % mod) % mod;
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, -1, 0, 1};
+        int moves = 0;
+        for (int k = 0; k < delRow.length; k++) {
+            if (maxMove > 0) {
+                int nRow = i + delRow[k];
+                int nCol = j + delCol[k];
+                moves += solve(nRow, nCol, maxMove - 1, m , n, dp) % mod;
+                moves %= mod;
+            }
+        }
+        return dp[i][j][maxMove] = moves % mod;
     }
 
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
