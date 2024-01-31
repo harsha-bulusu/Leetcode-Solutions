e9bfb237b025b249327b48654f60131d594e259c
harsha_bulusu <sriharsha16208@gmail.com>
2024-01-31 22:36:16 +0530

    Create CountingBits.java

---
+++ CountingBits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            int temp = i;
+            while (temp != 0) {
+                if ((temp & 1) == 1) count++;
+                temp >>= 1;
+            }
+            res[i] = count;
+        }
+
+        return res;
+    }
+}
