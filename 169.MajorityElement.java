5a91187d0aee5318c5cafacd62fffc8507032b79
harsha_bulusu <sriharsha16208@gmail.com>
2024-02-07 21:06:17 +0530

    Update 169.MajorityElement.java

--- 169.MajorityElement.java
+++ 169.MajorityElement.java
@@ -1,29 +1,46 @@
-import java.util.Map.*;
-
 class Solution {
     public int majorityElement(int[] nums) {
-        /**
-            Moores voting Algorithm
-            
-            step-1: Load element if count is 0
-            step-2: Visit next element and increment count if current element equals loaded element else decrement count
-            step-3: Repeat step-1 and step-2 until the entire array is traversed
-            step-4: The loaded element will be majority element
-         */
+        // Brute force : Linear search O(n ^ 2) SC : O(1)
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     int count = 0;
+        //     for (int j = 0; j < n; j++) {
+        //         if (nums[i] == nums[j]) count++;
+        //     }
+        //     if (count > n / 2) return nums[i];
+        // }
+        // return -1;
 
-        int count = 0, ele = 0;
-        for (int i=0; i<nums.length; i++) {
+        // Better solution : Hashing
+        // TC : O(nlogn) + O(n), SC: O(n)
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for (int i : nums) {
+        //     map.put(i, map.getOrDefault(i, 0) + 1);
+        // }
+
+        // int max = -1;
+        // int maxCount = 0;
+        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     int key = entry.getKey();
+        //     int count = entry.getValue();
+        //     if (count >  maxCount) {
+        //         max = key;
+        //         maxCount = count;
+        //     }
+        // }
+        // return max;
+
+        //Optimal : Moores voting algorithm
+        int count = 0;
+        int ele = 0;
+        for (int i : nums) {
             if (count == 0) {
-                ele = nums[i];
-            }
-
-            if (ele == nums[i]) {
+                ele = i;
                 count++;
-            } else {
-                count--;
             }
+            else if (i == ele) count++;
+            else count--;
         }
-
         return ele;
     }
 }
