f3e0a72c0456a53f8b4392f4652b49c6032f3a24
harsha_bulusu <sriharsha16208@gmail.com>
2024-02-08 08:39:12 +0530

    Update BestTimeToBuyAndSellAStock.java

--- BestTimeToBuyAndSellAStock.java
+++ BestTimeToBuyAndSellAStock.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int totalProfit = 0;
-        int min = prices[0];
-
-        for (int i = 1; i < prices.length; i++) {
-            int currProfit = prices[i] - min;
-            totalProfit = Math.max(currProfit, totalProfit);
-            min = Math.min(min, prices[i]);
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        int n = prices.length;
+        for (int i = 1; i < n; i++) {
+            int currProfit = prices[i] - minPrice;
+            maxProfit = Math.max(currProfit, maxProfit);
+            minPrice = Math.min(prices[i], minPrice);
         }
-
-        return totalProfit;
+        return maxProfit;
     }
 }
