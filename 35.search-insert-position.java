757aad162a3cfc9dda6f14eccb33e9eb1208706c
harsha_bulusu <sriharsha16208@gmail.com>
2024-02-13 10:07:08 +0530

    Update 35.search-insert-position.java

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -1,14 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for (int i=0; i<nums.length; i++) {
-            if (nums[i] < target) {
-                if (nums[i] == target) {
-                    return i;
-                }
+        //Binary Search
+        int n = nums.length;
+        int low = 0, high = n - 1; // defining the search space
+        int index = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= target) {
+                index = mid;
+                high = mid - 1;
             } else {
-                return i;
+                low = mid + 1;
             }
         }
-        return nums.length;
+        return index;
     }
 }
