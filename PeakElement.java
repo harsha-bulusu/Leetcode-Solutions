e25ec4d6627d45798d67c8101ba6d04af3343ecb
harsha_bulusu <sriharsha16208@gmail.com>
2024-02-15 18:18:20 +0530

    Create PeakElement.java

---
+++ PeakElement.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 0;
+
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 2] < nums[n - 1]) return n - 1;
+
+        // define search space
+        int low = 1, high = n - 2;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (nums[mid - 1] < nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
