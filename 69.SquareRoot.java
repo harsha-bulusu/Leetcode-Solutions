417ac4d0b12e312b6fe4e6928f900bdc95932cd1
harsha_bulusu <sriharsha16208@gmail.com>
2024-02-16 09:45:01 +0530

    Update 69.SquareRoot.java

--- 69.SquareRoot.java
+++ 69.SquareRoot.java
@@ -1,14 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1){
-            return x;
+        long X = x;
+        if (x == 0 || x == 1) return x;
+
+        long low = 0, high = X;
+        long ans = x;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if ((mid * mid) <= X) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        double prevN = -1;
-        double n = 1;
-        while (prevN != n) {
-            prevN = n;
-            n = 0.5 * (n + (x/n));
-        }
-        return (int)n;
+        return (int)ans;
     }
 }
