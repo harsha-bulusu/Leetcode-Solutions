2ad74e44098222bdf2f681cf0b3d4893d41532f1
harsha_bulusu <sriharsha16208@gmail.com>
2024-03-07 12:25:23 +0530

    Create BinaryTreeRightSideView.java

---
+++ BinaryTreeRightSideView.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        int val = root.val;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            if (node == null) {
+                res.add(val);
+                if (!queue.isEmpty()) {
+                    queue.add(null);
+                }
+            } else {
+                val = node.val;
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+}
